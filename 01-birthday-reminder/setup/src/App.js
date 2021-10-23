2ac83ca5ec0d0ae5cee156ad231e5bce655e2017
nadvolod <nadvolod@gmail.com>
2021-10-23 09:47:20 -0400

    static app setup

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,15 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder setup</h2>
+  return (
+    <main>
+      <section className='container'>
+        <h3>0 birthdays today</h3>
+        <List></List>
+        <button onClick={() => console.log('clicked')}>clear all</button>
+      </section>
+    </main>
+  )
 }
 
 export default App;
