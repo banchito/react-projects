d969400403b5c55e2659cf518f519d377cf383a3
nadvolod <nadvolod@gmail.com>
2022-01-03 20:05:48 +0100

    onClick() and onMouseOver()

--- 24-github-users/src/App.js
+++ 24-github-users/src/App.js
@@ -96,11 +96,34 @@ const books = [
 // used the {...book} operator to get all the properties of the book
 // then destructured in the parameters of the Book()
 function Book({ img, title, author }) {
+  // attribute, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = () => {
+    alert("hello world");
+  };
+
+  const complexExample = (author) => {
+    console.log(author);
+  };
+
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img src={img} alt=""></img>
-      <h1>{title}</h1>
+      {/* we're using an inline function to perform an onClick() */}
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        reference example
+      </button>
+      {/* we were able to pass a varable to our function */}
+      <button type="button" onClick={() => complexExample(author)}>
+        more complex example
+      </button>
     </article>
   );
 }
