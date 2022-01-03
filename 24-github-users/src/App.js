1824765b208d32d9e20dd17cd3e5a600a39daf0c
nadvolod <nadvolod@gmail.com>
2022-01-03 15:31:00 +0100

    amazon books project

--- 24-github-users/src/App.js
+++ 24-github-users/src/App.js
@@ -1,36 +1,96 @@
 import React from "react";
-import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+// JSX Rules
+//1. always needs to return a single element like a <div>
+//legal
+// const App = () => {
+//   return <h1>my arrow component</h1>;
+// };
+//illegal
+// const App = () => {
+//   return <h1>my arrow component</h1>
+//   <h1>2 elements returned</h1>;
+// };
+
+// Tip: don't place everything into a <div> as a div soup,
+// enclose stuff into appropriate elements
+
+//2. use camelCase for html attribute
+//3. use className instead of class
+//4. close every element
+
+//stateless functional component
+// function App() {
+//   // this is JSX
+//   return <h1>my first component</h1>;
+// }
+
+// An arrow component works the same as a
+// function component
+// const App = () => {
+//   return <h1>my arrow component</h1>;
+// };
+
+/**
+ * Nested Components
+ */
+
+// We can create a component that looks like this
+// const App = () => {
+//   return (
+//     <div>
+//       <h2>Nikolay</h2>
+//       <p>is a React guru</p>
+//     </div>
+//   );
+// };
+
+// And then wrap the JSX into new components and nest the components
+// and it will look exactly the same, see App below
+// const Person = () => {
+//   return <h2>Nikolay</h2>;
+// };
+
+// const Message = () => {
+//   return <p>is a React guru</p>;
+// };
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Person></Person>
+//       <Message></Message>
+//     </div>
+//   );
+// };
+
+const App = () => {
   return (
-    //It's typical to wrap the app in a Router so that
-    //we can have good page navigation
+    <section>
+      <Book></Book>
+    </section>
+  );
+};
 
-    //adding a path to a Route allows us to determine
-    //the endpoint of the component
-
-    //exact={true} tells Dashboard to only appear on
-    // localhost/ and not on localhost/*
-
-    //<Switch> will render the first component that is found
-    //and not rendering the Error page on every Route
-    <AuthWrapper>
-      <Router>
-        <Switch>
-          <PrivateRoute path="/" exact={true}>
-            <Dashboard></Dashboard>
-          </PrivateRoute>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="*">
-            <Error></Error>
-          </Route>
-        </Switch>
-      </Router>
-    </AuthWrapper>
+function Book() {
+  return (
+    <article>
+      <Image></Image>
+      <Title></Title>
+      <Author></Author>
+    </article>
   );
 }
+
+// an implicit return
+const Image = () => <img src="../images/preloader.gif" alt=""></img>;
+
+const Title = () => {
+  return <h1>I love you to the moon</h1>;
+};
+
+const Author = () => {
+  return <h4>Amelia H</h4>;
+};
 
 export default App;
